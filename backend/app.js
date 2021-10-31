9aa42a54492171e61dd30ea8c53db3c0e5d99e9c
KoshkaKsu <ksenia.kuzn@mail.ru>
2021-10-31 17:47:53 +0300

    frontend

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ const app = express();
 
 const corsOptions = {
   origin: ['http://localhost:3000', 'https://mesto.autors.nomoredomains.work'],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Methods', 'Access-Control-Request-Headers'],
   credentials: true,
   enablePreflight: true,
